4dbdb94d6d8c06e6664077326a61762242a589e6
utkucnay <utkumay57@gmail.com>
2022-04-03 03:30:47 +0300

    SüperDüper Şeyler
    NavMeshPlus lazım yapay zeka için

--- Assets/1-Scripts/Enemy.cs
+++ Assets/1-Scripts/Enemy.cs
@@ -1,14 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour
 {
+    public Transform Target;
+
     private int Health;
     private float Speed;
+    private NavMeshAgent navMeshAgent;
 
+    private void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        navMeshAgent.updateRotation = false;
+        navMeshAgent.updateUpAxis = false;
+    }
 
-
+    private void Update()
+    {
+        navMeshAgent.SetDestination(Target.position);
+    }
     public void DestroyEnemy()
     {
         Destroy(this.gameObject);
