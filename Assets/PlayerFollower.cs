51f6e9e05ee2c2916b48131cedee51b8426510a4
Taha Yasin Mimarlar <taharey.com59@hotmail.com>
2022-04-03 15:06:51 +0300

    time controller

--- Assets/PlayerFollower.cs
+++ Assets/PlayerFollower.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class PlayerFollower : MonoBehaviour
 {
+
+    GameObject playerRef;
+    Vector3 startPos;
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerRef = GameObject.FindGameObjectWithTag("Player");
+        startPos = playerRef.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        gameObject.transform.position = (playerRef.transform.position - startPos)/5;
     }
 }
